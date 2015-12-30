e09143292378d5a27754c5934200372203efcbc0
Chong Liu <lc87624@gmail.com>
2015-12-30 17:42:12 +0800

    引入weixin-java-tools框架

--- src/main/java/me/gemoji/weixin/otakuworld/servlet/WeixinServlet.java
+++ src/main/java/me/gemoji/weixin/otakuworld/servlet/WeixinServlet.java
@@ -1,102 +1,142 @@
 package me.gemoji.weixin.otakuworld.servlet;
 
 
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import me.gemoji.weixin.otakuworld.bean.WeixinNewsArticle;
-import me.gemoji.weixin.otakuworld.bean.WeixinNewsMessage;
-import me.gemoji.weixin.otakuworld.bean.WeixinTextMessage;
-
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
+import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.common.session.WxSessionManager;
+import me.chanjar.weixin.mp.api.*;
+import me.chanjar.weixin.mp.bean.*;
 import org.apache.commons.lang3.StringUtils;
 
-@SuppressWarnings("serial")
 public class WeixinServlet extends HttpServlet{
 
+	private static final String WEIXIN_TOKEN = "otakuworld";
+	private static final String WEIXIN_APP_ID = "wxaa1abd389bab6ff1";
+	private static final String WEIXIN_APP_SECRET = "d4624c36b6795d1d99dcf0547af5443d";
+
+	protected WxMpInMemoryConfigStorage wxMpConfigStorage;
+	protected WxMpService wxMpService;
+	protected WxMpMessageRouter wxMpMessageRouter;
+
+	@Override public void init() throws ServletException {
+		super.init();
+
+		wxMpConfigStorage = new WxMpInMemoryConfigStorage();
+		wxMpConfigStorage.setAppId(WEIXIN_APP_ID); // 设置微信公众号的appid
+		wxMpConfigStorage.setSecret(WEIXIN_APP_SECRET); // 设置微信公众号的app corpSecret
+		wxMpConfigStorage.setToken(WEIXIN_TOKEN); // 设置微信公众号的token
+		//wxMpConfigStorage.setAesKey("..."); // 设置微信公众号的EncodingAESKey
+
+		wxMpService = new WxMpServiceImpl();
+		wxMpService.setWxMpConfigStorage(wxMpConfigStorage);
+
+		WxMpMessageHandler cosHandler = new WxMpMessageHandler() {
+			@Override
+			public WxMpXmlOutMessage handle(WxMpXmlMessage wxMpXmlMessage, Map<String, Object> map,
+											WxMpService wxMpService, WxSessionManager wxSessionManager) throws WxErrorException {
+
+                WxMpXmlOutNewsMessage.Item article = new WxMpXmlOutNewsMessage.Item();
+                article.setTitle("Akali(英雄联盟) VS Sona(英雄联盟)");
+                article.setDescription("新鲜出炉的cos美图两张，各位看官赶紧投票吧~");
+                article.setPicUrl("http://ww2.sinaimg.cn/large/dd7aac41jw1e7j3w325u5j20kk0d5gqo.jpg");
+                article.setUrl("http://120.25.64.81/cos.html");
+
+
+				return WxMpXmlOutMessage.NEWS()
+                            .fromUser(wxMpXmlMessage.getToUserName())
+                            .toUser(wxMpXmlMessage.getFromUserName())
+                            .addArticle(article)
+                            .build();
+			}
+		};
+
+        WxMpMessageHandler defaultHandler = new WxMpMessageHandler() {
+            @Override
+            public WxMpXmlOutMessage handle(WxMpXmlMessage wxMpXmlMessage, Map<String, Object> map,
+                                            WxMpService wxMpService, WxSessionManager wxSessionManager) throws WxErrorException {
+
+                String content = "小宅我是复读机星人，貌似你刚说的是：“" + wxMpXmlMessage.getContent() + "”吧？那是个什么东西？有节操么？";
+
+                return WxMpXmlOutMessage.TEXT()
+                        .fromUser(wxMpXmlMessage.getToUserName())
+                        .toUser(wxMpXmlMessage.getFromUserName())
+                        .content(content)
+                        .build();
+            }
+        };
+
+		wxMpMessageRouter = new WxMpMessageRouter(wxMpService);
+
+		wxMpMessageRouter
+				.rule()
+				.async(false)
+				.content("cos") // 拦截内容为“cos”的消息
+				.handler(cosHandler)
+				.end();
+
+        wxMpMessageRouter
+                .rule()
+                .handler(defaultHandler)
+                .end();
+
+	}
+
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
+
 		resp.setContentType("text/html; charset=UTF-8");
 		resp.setCharacterEncoding("UTF-8");
 		PrintWriter out = resp.getWriter();
-		
-		String signatrue = req.getParameter("signature");
+
+		String signature = req.getParameter("signature");
 		String timestamp = req.getParameter("timestamp");
 		String nonce = req.getParameter("nonce");
-		String echostr = req.getParameter("echostr");
-		
-		if(signatrue == null || timestamp == null || 
-				nonce == null || echostr == null){
+
+		if(!wxMpService.checkSignature(timestamp, nonce, signature)){
 			out.print("微信君，貌似你打开的方式不对呦。。。");
 			return;
 		}
-		
-		String token = "otakuworld";
-		List<String> list = Arrays.asList(new String[]{token, timestamp, nonce});
-		Collections.sort(list);
-		
-		String raw = StringUtils.join(list, "");
-		String encrypt = DigestUtils.sha1Hex(raw);
-		
-		if(encrypt.equals(signatrue)){
+
+		String echostr = req.getParameter("echostr");
+		if(StringUtils.isNotBlank(echostr)){
 			out.print(echostr);
+			return;
 		}
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		
-		resp.setContentType("text/xml; charset=UTF-8");
-		resp.setCharacterEncoding("UTF-8");
-		PrintWriter out = resp.getWriter();
-		
-		String input = IOUtils.toString(req.getInputStream(), "utf8");
-		WeixinTextMessage msgIn = new WeixinTextMessage(input);
-		
-		String content = "小宅我是复读机星人，貌似你刚说的是：“" + msgIn.getContent() + "”吧？那是个什么东西？有节操么？";
-		
-		/*if("cos".equals(msgIn.getContent())){
-			String img1 = "http://tiny.cc/3qqn1w";
-			String img2 = "http://tiny.cc/xsqn1w";
-			content = img1 + "\n" + img2;
-			WeixinTextMessage msgOut = new WeixinTextMessage(msgIn.getFromUser(), msgIn.getToUser(), content);
-			
-			out.print(msgOut.toXmlStr());
-		}*/
-		
-		if("cos".equals(msgIn.getContent())){
-			WeixinNewsMessage news = new WeixinNewsMessage(msgIn.getFromUser(), msgIn.getToUser());
-			String img = "http://tiny.cc/3qqn1w";
-			String title = "Akali(英雄联盟) VS Sona(英雄联盟)";
-			String description = "新鲜出炉的cos美图两张，各位看官赶紧投票吧~";
-			WeixinNewsArticle article = new WeixinNewsArticle(title, description, img, "http://otakuworld.duapp.com/cos.html");
-			news.addArticle(article);
-			content = news.toXmlStr();
-			
-			out.print(news.toXmlStr());
-		}else{
-			WeixinTextMessage msgOut = new WeixinTextMessage(msgIn.getFromUser(), msgIn.getToUser(), content);
-			out.print(msgOut.toXmlStr());
+
+		String encryptType = StringUtils.isBlank(req.getParameter("encrypt_type")) ?
+				"raw" :
+				req.getParameter("encrypt_type");
+
+		if ("raw".equals(encryptType)) {
+			// 明文传输的消息
+			WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(req.getInputStream());
+			WxMpXmlOutMessage outMessage = wxMpMessageRouter.route(inMessage);
+			resp.getWriter().write(outMessage.toXml());
+			return;
 		}
+
+		if ("aes".equals(encryptType)) {
+			// 是aes加密的消息
+			String msgSignature = req.getParameter("msg_signature");
+			WxMpXmlMessage inMessage = WxMpXmlMessage.fromEncryptedXml(req.getInputStream(), wxMpConfigStorage, timestamp, nonce, msgSignature);
+			WxMpXmlOutMessage outMessage = wxMpMessageRouter.route(inMessage);
+			resp.getWriter().write(outMessage.toEncryptedXml(wxMpConfigStorage));
+			return;
+		}
+
+		resp.getWriter().println("不可识别的加密类型");
+		return;
+
 	}
-	
-	public static void main(String[] args) throws IOException {
-		String input = FileUtils.readFileToString(new File("C://test.txt"), "UTF-8");
-		System.out.println(input);
-		System.out.println(new WeixinTextMessage(input).toXmlStr());
-	}
+
 }
